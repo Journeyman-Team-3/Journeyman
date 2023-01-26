2dcbfeb5f5eaeca6d6e64fad246c48709719b495
Joe <b1085143@live.tees.ac.uk>
2023-01-26 11:43:00 +0000

    Project Files

---
+++ Source/Journeyman/Journeyman.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Journeyman.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Journeyman, "Journeyman" );
+ 
---
+++ Source/Journeyman/JourneymanGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "JourneymanGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AJourneymanGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AJourneymanGameMode();
+};
+
+
+
