1ff82edac13413e115ffec2221e0193147efc5d7
Jonny <jonathan.braine2001@gmail.com>
2023-02-07 18:24:19 +0000

    Added Clean.bat

---
+++ clean.bat
@@ -0,0 +1,13 @@
+del /s /q Script\*.*
+del /s /q Binaries
+del /s /q .vs\*.*
+del /s /q Intermediate\*.*
+del /s /q Saved\*.*
+del /s /q DerivedDataCache\*.*
+
+rd /s /q Script
+rd /s /q .vs
+rd /s /q Binaries
+rd /s /q Intermediate
+rd /s /q Saved
+rd /s /q DerivedDataCache
