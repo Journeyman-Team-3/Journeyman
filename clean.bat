89a06e5a7549f2bbb604407b8f140ff5860426d5
Samwheeler9 <A0104986@live.tees.ac.uk>
2023-02-08 11:41:04 +0000

    Updating Branch from main

---
+++ clean.bat
@@ -0,0 +1,11 @@
+del /s /q Script\*.*
+del /s /q .vs\*.*
+del /s /q Intermediate\*.*
+del /s /q Saved\*.*
+del /s /q DerivedDataCache\*.*
+
+rd /s /q Script
+rd /s /q .vs
+rd /s /q Intermediate
+rd /s /q Saved
+rd /s /q DerivedDataCache
