f5953c482d9eec114a505b4540bb124b97cdf836
Noah Threlfall-Holmes <124095430+NoahThrelfall-Holmes@users.noreply.github.com>
2023-02-15 18:31:28 +0000

    Added Base Classes
    Added Entity, Weapon, and Enemy C++ classes

---
+++ Source/Journeyman/Enemy.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Entity.h"
+#include "Enemy.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class JOURNEYMAN_API AEnemy : public AEntity
+{
+	GENERATED_BODY()
+	
+};
