ed3798f3825d901cec9d276e3ecb64c03e25ae2b
KevX501 <b1347652@live.tees.ac.uk>
2023-03-27 17:25:03 +0100

    Added button prompts for interaction & checkpoint reached visual

--- Source/Journeyman/AttackComponent.h
+++ Source/Journeyman/AttackComponent.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "AttackSwingCapsule.h"
 #include "RangeProjectile.h"
+#include "AIController.h"
 #include "Components/ActorComponent.h"
 #include "Components/ArrowComponent.h"
 #include "AttackComponent.generated.h"
@@ -16,60 +17,40 @@ class JOURNEYMAN_API UAttackComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-// VARIABLES
 public:	
 	// Sets default values for this component's properties
 	UAttackComponent();
 
-	// Swing
-	UPROPERTY(EditAnywhere, Category=Components)
-	TSubclassOf<AAttackSwingCapsule> SwingCollisionClass;
-
-	// Range
-	TSubclassOf<ARangeProjectile> RangeProjectileClass;
-
-private:
-	// Swing
-	bool bIsSwinging;
-	AActor* OwningActor;
-	
-	UPROPERTY()
-	AAttackSwingCapsule* SwingCollision;
-
-protected:
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Gameplay)
-	FVector SpawnOffset;
-
-	UPROPERTY(EditDefaultsOnly, Category=Projectile)
-	TSubclassOf<class ARangeProjectile> ProjectileClass;
-
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Components)
-	UArrowComponent* ProjectileSpawnLocation;
-
-	UPROPERTY(EditAnywhere, Category=Gameplay)
-	UAnimMontage* AttackAnimation;
-
-// FUNCTIONS
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 	UFUNCTION(BlueprintCallable)
 	void Attack(TSubclassOf<AWeapon> AttackActor);
 
+	void TriggerSword();
+	void StopTriggerSword();
+	
+	
+
 private:
+	AActor* OwningActor;
+	
+	bool bAttackOnce = true;
+
+	void SwordLineTrace();
+	FTimerHandle SwordSwingTimerHandle;
+
 	void SwingAttack(TSubclassOf<AWeapon> Weapon);
-	void RangeAttack(TSubclassOf<AActor> Projectile);
-	bool IsBetween(float CurrentValue, float MaxValue, float MarginForError);
+	void RangeAttack(TSubclassOf<AWeapon> Projectile);
 
-	// Swing
-	float FindMaxRotation(float StartRotation);
-
+	USkeletalMeshComponent* WeaponMesh;
+	AWeapon* CurrentWeapon = nullptr;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-
-		
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Components)
+	UArrowComponent* ProjectileSpawnLocation;
+	
 };
